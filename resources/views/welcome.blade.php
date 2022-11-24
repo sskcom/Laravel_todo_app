0b536a81f4f8ead147ee9fb4aa90c06b7ac68e63
sskcom <sskcomjp08131@gmail.com>
2022-11-25 08:13:51 +0900

    認証画面を追加、ダッシュボードにtodo機能を追加

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todo</title>
+
+    @vite('resources/css/app.css')
+</head>
+
+<body class="flex flex-col min-h-[100vh]">
+    <header class="bg-slate-800">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6">
+            <div class="py-6">
+                <p class="text-white text-xl">Todoアプリ</p>
+            </div>
+        </div>
+    </header>
+
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100  sm:items-center py-4 sm:pt-0">
+            @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/dboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">ログイン</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">新規登録</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+   
+    <!-- <footer class="bg-slate-800">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6">
+            <div class="py-4 text-center">
+                <p class="text-white text-sm">Todoアプリ</p>
+            </div>
+        </div>
+    </footer> -->
+    
+</body>
+
+</html>
